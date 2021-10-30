b93f54b55e420c6d4fe61e377fb698de947e18a2
Pilke95 <nikolapilke@gmail.com>
2021-10-30 20:02:28 +0200

    Changed gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer');
+
 
 function buildStyles() {
     return src('sass/main.scss')
         .pipe(sass())
-        .pipe(dest('css/main.css'))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['sass/main.scss'], buildStyles)
+    watch(['sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
